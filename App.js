8d175aa461f8c8cc2ffa0022093698917ac30056
Hakan Akın <cehakanakin@gmail.com>
2020-03-22 21:19:53 +0300

    Created header, card components and StartGameScreen

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import Header from "./components/Header";
+import StartGameScreen from './screens/StartGameScreen'
 
 export default function App() {
   return (
-    <View>
-
+    <View style={styles.screen}>
+      <Header title="Guess a Number" />
+      <StartGameScreen />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  screen: {
+    flex: 1
+  }
 });
